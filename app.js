ec6a25bd46e56cae871ee9ae8da140c8596013cf
bartoginski <ogitoziomal@gmail.com>
2020-06-19 14:05:14 +0200

    correct comments

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+//conts querySelectors
 const button = document.querySelector(".random-btn");
 const colorHeader = document.querySelector(".color-header");
 const body = document.querySelector(".background");
+
 //* HEX RGB
 // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 // A, B, C, D, E, F
 
 const hexRGB = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
+//events listeners
 button.addEventListener("click", colorGenerator);
 
+//functions
 function colorGenerator() {
     let color = ["#"];
     for (let i = 0; i <= 5; i++) {
