0f7ff22cece32b94a8831b6a206f2e3152117bc0
bartoginski <ogitoziomal@gmail.com>
2020-06-19 13:35:59 +0200

    complete project

---
+++ app.js
@@ -0,0 +1,26 @@
+const button = document.querySelector(".random-btn");
+const colorHeader = document.querySelector(".color-header");
+const body = document.querySelector(".background");
+//* HEX RGB
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+// A, B, C, D, E, F
+
+const hexRGB = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+button.addEventListener("click", colorGenerator);
+
+function colorGenerator() {
+    let color = ["#"];
+    for (let i = 0; i <= 5; i++) {
+        let randomNum = Math.round(Math.random() * hexRGB.length - 1);
+        let colorNum = hexRGB[randomNum];
+        color.push(colorNum);
+    }
+    let generatedColor = color.join("");
+    colorChanges(generatedColor);
+}
+
+function colorChanges(generatedColor) {
+    colorHeader.innerText = generatedColor;
+    body.style.backgroundColor = generatedColor;
+}
